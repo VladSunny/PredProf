2d846a91f35c4b67d6ef21edc1ecad4366e6ea69
vladg00dman <vladislavsunny@gmail.com>
2026-01-30 12:39:32 +0300

    daisy ui

--- school-meal-system/src/App.jsx
+++ school-meal-system/src/App.jsx
@@ -1,11 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="flex flex-col items-center w-full">
@@ -13,6 +6,8 @@ function App() {
           <p className="text-3xl">Test</p>
         </div>
       </div>
+
+      <input type="file" className="file-input" />
     </>
   );
 }
